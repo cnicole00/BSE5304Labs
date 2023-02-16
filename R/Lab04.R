fefc1dbf356fd3211b3cb36f5c1e58805d042a1e
cnicole00 <cnicole00@vt.edu>
2023-02-16 13:57:29 -0500

    Adding soil wetting/drying/drying above capacity functions

---
+++ R/Lab04.R
@@ -0,0 +1,8 @@
+
+# The solution to last week's lab solution: 
+# https://raw.githubusercontent.com/vtdrfuka/BSE5304Labs/main/R/Lab03SolDRF.R
+# becomes: 
+url="https://raw.githubusercontent.com/vtdrfuka/BSE5304Labs/main/R/Lab03SolDRF.R"
+# This will grab the solution for last weeks Lab03 Homework
+download.file(url,"Lab03_HW_Solution.R")
+file.edit("Lab03_HW_Solution.R")
